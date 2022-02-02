6a7ade3d400e11b8540a349aaf7f42a2138d1ea7
Vinicius Aquino <vinicius.dias2020@engenharia.ufjf.br>
2022-02-02 16:24:21 -0300

    Adicionado os exercicios sobre if/else

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -19,3 +19,16 @@ const area = base * height;
 const perimeter = (base * 2) + (height * 2)
 
 console.log(base, height, area, perimeter);
+
+
+// Exercicio 3 - If/else
+
+const nota = 50;
+
+if(nota >= 80){
+    console.log("Parabéns, você foi aprovada(o)!")
+}else if (nota <= 80 && nota >= 60){
+    console.log("Você está na nossa lista de espera")
+}else{
+    console.log("Você foi reprovado!")
+}
