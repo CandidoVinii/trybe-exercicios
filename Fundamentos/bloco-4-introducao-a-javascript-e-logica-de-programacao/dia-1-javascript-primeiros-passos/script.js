1bbbd6efa4e73bf240dff5a250c450a1dc569dfb
Vinicius Aquino <vinicius.dias2020@engenharia.ufjf.br>
2022-02-02 16:43:22 -0300

    Adicionado exercicios operadores logicos &&

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -26,9 +26,34 @@ console.log(base, height, area, perimeter);
 const nota = 50;
 
 if(nota >= 80){
-    console.log("Parabéns, você foi aprovada(o)!")
+   console.log("Parabéns, você foi aprovada(o)!")
 }else if (nota <= 80 && nota >= 60){
-    console.log("Você está na nossa lista de espera")
+  console.log("Você está na nossa lista de espera")
 }else{
-    console.log("Você foi reprovado!")
+  console.log("Você foi reprovado!")
 }
+
+//Exercicio 4 - Operadores lógicos
+
+const currentHour = 16;
+let message = '';
+
+if(currentHour >= 22){
+
+    message = "Não deveríamos comer nada, é hora de dormir";
+
+}
+else if (currentHour >= 18 && currentHour <= 22){
+    message = "Rango da noite, vamos jantar :D";
+}
+else if (currentHour >= 14 && currentHour <= 18){
+    message = "Vamos fazer um bolo pro café da tarde?";
+}
+else if (currentHour >= 11 && currentHour <= 14){
+    message = "Hora do almoço!!!";
+}
+else if (currentHour >= 4 && currentHour <= 11){
+    message = "Hmmm, cheiro de café recém passado";
+}
+
+    console.log (message)
