0f3ee17f436707fc7b653cdfd03dbb1704f88fca
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-03-09 18:33:30 -0300

    Resolvid erro de identação

--- Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio3.js
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio3.js
@@ -3,12 +3,12 @@ const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
 
 const comparaRespostas = (respostaCerta, repostaAluno) => {
-    if (respostaCerta === repostaAluno){
-        return 1;
-    } if (repostaAluno === 'N.A'){
-        return 0;
-    }
-    return -0.5;
+	if (respostaCerta === repostaAluno){
+		return 1;
+} if (repostaAluno === 'N.A'){
+		return 0;
+}
+		return -0.5;
 };
 
 const conta = (respostaCerta, repostaAluno, adiciona) => {
