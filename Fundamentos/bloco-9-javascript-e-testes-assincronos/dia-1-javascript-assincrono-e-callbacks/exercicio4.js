5a470642649f008d5dde19f4db66f235b2d620f1
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-03-22 19:43:02 -0300

    resolvido exercicio 4

---
+++ Fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicio4.js
@@ -0,0 +1,16 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = () => {
+  setTimeout(() => {
+  const pegaTemp = getMarsTemperature();
+  console.log(pegaTemp);
+  }, messageDelay()); 
+}
+
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
